3fc159d845a37d9119f4bdc6e5b26e056fdbd240
Dean Wampler <dean@concurrentthought.com>
2019-01-28 14:36:49 -0600

    Extracted ServingResult into its own file, since it's referenced in the README.

---
+++ model/src/main/scala/com/lightbend/modelserving/model/ServingResult.scala
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2019  Lightbend
+ *
+ * This file is part of ModelServing-tutorial
+ *
+ * ModelServing-tutorial is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.lightbend.modelserving.model
+
+/**
+ * Model serving result definition.
+ */
+case class ServingResult[RESULT](
+  name : String,
+  dataType : String = "",
+  duration : Long = 0,
+  result: RESULT = null.asInstanceOf[RESULT])
